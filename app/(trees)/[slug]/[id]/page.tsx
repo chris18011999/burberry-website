a08863e598accc5ce14d8b91ee9b165e9d22739a
Chris Meurink <chris@Chriss-MacBook-Air.local>
2024-05-03 12:55:07 +0200

    generateStaticParams for category pages

--- app/(trees)/[slug]/[id]/page.tsx
+++ app/(trees)/[slug]/[id]/page.tsx
@@ -1,19 +1,37 @@
-import ProductCard from "@/components/product-card";
-import { ProductFilters } from "@/components/product-filters";
 import { ProductGrid } from "@/components/product-grid";
 import fetcher from "@/data/fetcher";
+
+export async function generateStaticParams({
+  params,
+}: {
+  params: { slug: string; id: number };
+}) {
+  const { products } =
+    await fetcher.getProductsByTree(params.id);
+    const filters = await fetcher.getFiltersByTree(params.id);
+
+  return {
+    products,
+    filters
+  };
+}
 
 export default async function CategoryPage({
   params,
 }: {
   params: { slug: string; id: number };
 }) {
-  const { products, total_found, search_term } = await fetcher.getProductsByTree(params.id);
+  const { products, total_found, search_term } =
+    await fetcher.getProductsByTree(params.id);
   const filters = await fetcher.getFiltersByTree(params.id);
 
   return (
     <main className="container mx-auto">
-      <ProductGrid products={products} filters={filters} total_found={total_found}></ProductGrid>
+      <ProductGrid
+        products={products}
+        filters={filters}
+        total_found={total_found}
+      ></ProductGrid>
     </main>
   );
 }
